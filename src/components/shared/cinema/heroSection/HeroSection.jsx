a3c92bff1270d82b81371ca8df381ca6aa86dd5e
Sacre-MBIKU <sajombiku@gmail.com>
2021-05-05 17:48:53 +0100

    refactor header props

--- src/components/shared/cinema/heroSection/HeroSection.jsx
+++ src/components/shared/cinema/heroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import HeroSectionDecoration from "../../../../layouts/home/heroSection/HeroSectionDecoration";
-import bannerSection from "../../../../assets/backgrounds/heroSectionCinema.jpg";
+import bannerSection from "../../../../assets/backgrounds/hero1.png";
 const HeroSectionCinema = () => (
   <HeroSectionDecoration banner={bannerSection}></HeroSectionDecoration>
 );
